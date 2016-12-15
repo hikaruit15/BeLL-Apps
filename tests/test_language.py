88e85382ebfc66b0e278dc75c10792051b640f96
Dinh Bao <hikaruit15@gmail.com>
2016-12-15 02:02:53 -0500

    add try except

--- tests/test_language.py
+++ tests/test_language.py
@@ -7,12 +7,13 @@ from base_case import BaseCase
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import StaleElementReferenceException
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 from time import sleep
-
+from time import time
 @on_platforms(browsers)
 class LanguageTest(BaseCase):
 
@@ -23,17 +24,38 @@ class LanguageTest(BaseCase):
         driver.get("http://127.0.0.1:5981/apps/_design/bell/MyApp/index.html")
         # test all languages
         languages = ["Arabic", "English", "Spanish", "Urdu"]
-        logins = ["تسجيل الدخول", "Sign In", "Crear cuenta", "سائن ان کریں"]
+        logins = ["دخول", "Login", "Iniciar sesión", "لاگ ان"]
+
+       # logins = ["تسجيل الدخول", "Sign In", "Crear cuenta", "سائن ان کریں"]
         for i in range(len(languages)):
             WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, "onLoginLanguage")))
-            dropdown = Select(driver.find_element_by_id("onLoginLanguage"))
-            old_button = driver.find_element_by_id("formButton")
-            dropdown.select_by_value(languages[i])
+            while True:
+                try:
+                    dropdown = Select(driver.find_element_by_id('onLoginLanguage'))
+                    dropdown.select_by_value(languages[i])
+                    break
+                except StaleElementReferenceException:
+                    print("Stale select")
 
-            sleep(10)
-            WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "formButton")))   
+
+            # wait for page reload 2 times
+            WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.TAG_NAME, "html")))   
+            WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.TAG_NAME, "html")))   
+            WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.TAG_NAME, "html")))   
+            WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.TAG_NAME, "html")))               
             
-            actual = driver.find_element_by_id('formButton').text
+            WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH, '//label[@for="c50_login"]')))   
+
+            actual = ""
+            while True:
+                try:
+                    actual = driver.find_element_by_xpath('//label[@for="c50_login"]').text
+                    break
+                except StaleElementReferenceException:
+                    print("Stale c50_login")
+
+            
+            value_option = languages[i]
             expected = logins[i]
             self.assertEqual(actual, expected)
             
