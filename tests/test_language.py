31d42d8bb4a4b229a4eb93afbbdd94a1a05b9318
Dinh Bao <hikaruit15@gmail.com>
2017-01-22 17:38:11 -0700

    refactoring code

--- tests/test_language.py
+++ tests/test_language.py
@@ -1,8 +1,5 @@
 import unittest
-<<<<<<< HEAD
-=======
 import bell
->>>>>>> upstream/dev
 
 from base_case import on_platforms
 from base_case import browsers
@@ -10,17 +7,6 @@ from base_case import BaseCase
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-<<<<<<< HEAD
-from selenium.common.exceptions import TimeoutException
-from selenium.common.exceptions import StaleElementReferenceException
-from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver.support.ui import Select
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
-from time import sleep
-from time import time
-=======
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import TimeoutException
@@ -30,7 +16,6 @@ from selenium.webdriver.support import expected_conditions as EC
 
 from time import sleep
 
->>>>>>> upstream/dev
 @on_platforms(browsers)
 class LanguageTest(BaseCase):
 
@@ -38,56 +23,7 @@ class LanguageTest(BaseCase):
         driver = self.driver
         
         # go to homepage
-<<<<<<< HEAD
-        driver.get("http://127.0.0.1:5981/apps/_design/bell/MyApp/index.html")
-        # test all languages
-        languages = ["Arabic", "English", "Spanish", "Urdu"]
-       # logins = ["دخول", "Login", "Iniciar sesión", "لاگ ان"]
 
-        logins = ["تسجيل الدخول", "Sign In", "Crear cuenta", "سائن ان کریں"]
-
-        text_lan = ["العربية", "English", "Español", "اردو"]
-        for i in range(len(languages)):
-            WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, "onLoginLanguage")))
-            while True:
-                try:
-                    #dropdown = Select(driver.find_element_by_id('onLoginLanguage'))
-                    #dropdown.select_by_value(languages[i])
-                   # driver.find_element_by_id('onLoginLanguage').send_keys(text_lan[i])
-                    driver.find_element_by_xpath('//select[@id="onLoginLanguage"]/option[@value="' + languages[i] + '"]').click()
-                    break
-                except StaleElementReferenceException:
-                    print("Stale select" + str(i))
-
-
-            # wait for page reload 2 times
-            #WebDriverWait(driver, 60).until(EC.invisibility_of_element_located((By.TAG_NAME, "html")))   
-            #WebDriverWait(driver, 60).until(EC.visibility_of_element_located((By.TAG_NAME, "html")))   
-            #WebDriverWait(driver, 60).until(EC.invisibility_of_element_located((By.TAG_NAME, "html")))   
-            #WebDriverWait(driver, 60).until(EC.visibility_of_element_located((By.TAG_NAME, "html")))               
-            
-           
-
-            sleep(80)
-            WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, 'formButton')))   
-
-            actual = ""
-            
-            while True:
-                try:
-                    actual = driver.find_element_by_xpath('//a[@id="formButton"]').text
-                    print("actual : " + actual)
-                    break
-                  #  if actual == logins[i]:
-                   #     break
-                except StaleElementReferenceException:
-                    print("Stale formButton" + str(i))
-                except NoSuchElementException:
-                    print("No such Element" + str(i))
-                    sleep(1)
-            
-            expected = logins[i]
-=======
         driver.get(bell.get_url())
         # test all languages
         languages = ["Arabic", "English", "Spanish", "Urdu"]
@@ -102,14 +38,7 @@ class LanguageTest(BaseCase):
             
             actual = driver.find_element_by_xpath("//label[contains(@for, 'login')]").text
             expected = login
->>>>>>> upstream/dev
             self.assertEqual(actual, expected)
             
-
-
 if __name__ == "__main__":
     unittest.main()
-<<<<<<< HEAD
-=======
-
->>>>>>> upstream/dev
